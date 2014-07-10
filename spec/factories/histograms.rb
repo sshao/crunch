a51a79c25e00cfebb4e1c5225c4555896e3cfcad
Sophia Shao <sshao@andrew.cmu.edu>
2014-07-09 23:47:51 -0500

    remove histogram field from test factory
    Histogram attribute should be created by Histogram model,
    rather than assigned

--- spec/factories/histograms.rb
+++ spec/factories/histograms.rb
@@ -1,14 +1,6 @@
 FactoryGirl.define do
-  hist_hash = {"#EA3556" => 100,
-               "#61D2D6" => 200,
-               "#EDE5E2" => 300,
-               "#ED146F" => 400,
-               "#EDDE45" => 500,
-               "#9BF0E9" => 600}
-
   factory :histogram do 
     username      "arrow"
-    histogram     hist_hash
     source_ts     1404670749
     dataset_size  5
   end
