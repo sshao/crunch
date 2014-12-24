e43f294a46803afc27863c5ff3bf2ee0ff9df837
sshao <sshao@users.noreply.github.com>
2014-12-24 12:29:06 -0600

    refactored helpers

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,12 +1,17 @@
 module Helpers
   TEST_PULL_LIMIT = 3
+  API_URI = "api.tumblr.com/v2/blog"
+
+  def api_uri_for(username)
+    "#{API_URI}/#{username}.tumblr.com"
+  end
 
   def photo_uri(username)
-    %r{api.tumblr.com/v2/blog/#{username}.tumblr.com/posts/photo\?.*&limit=#{PULL_LIMIT}}
+    %r{#{api_uri_for(username)}/posts/photo\?.*&limit=#{PULL_LIMIT}}
   end
 
   def info_uri(username)
-    %r{api.tumblr.com/v2/blog/#{username}.tumblr.com/info}
+    %r{#{api_uri_for(username)}/info}
   end
 
   def headers
@@ -14,17 +19,28 @@ module Helpers
   end
 
   def stub_photo_request(username)
-    body = File.open(File.join(fixture_path, "#{username}_photos_success.json")).read
-    stub_request(:get, photo_uri(username)).to_return(status: 200, headers: headers, body: body)
+    fp = File.join(fixture_path, "#{username}_photos_success.json")
+    body = File.read(fp)
+
+    stub_request(:get, photo_uri(username)).to_return(status: 200,
+                                                      headers: headers,
+                                                      body: body)
   end
 
   def stub_info_request(username)
-    body = File.open(File.join(fixture_path, "#{username}_info_success.json")).read
-    stub_request(:get, info_uri(username)).to_return(status: 200, headers: headers, body: body)
+    fp = File.join(fixture_path, "#{username}_info_success.json")
+    body = File.read(fp)
+
+    stub_request(:get, info_uri(username)).to_return(status: 200,
+                                                     headers: headers,
+                                                     body: body)
   end
 
   def stub_info_request_undefined_user(username)
     body = '{"meta":{"status":404,"msg":"Not Found"},"response":[]}'
-    stub_request(:get, info_uri(username)).to_return(status: 404, headers: headers, body: body)
+
+    stub_request(:get, info_uri(username)).to_return(status: 404,
+                                                     headers: headers,
+                                                     body: body)
   end
 end
