035023abb123d838a23e0cb96e38c7802d53af00
sshao <sshao@users.noreply.github.com>
2014-12-26 14:38:40 -0600

    removed source_ts from histogram factory

--- spec/factories/histograms.rb
+++ spec/factories/histograms.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
-  factory :histogram do 
+  factory :histogram do
     username      "arrow"
-    # FIXME source_ts should also be computed... as should dataset_size
-    source_ts     1404670749
   end
 
   factory :invalid_histogram, parent: :histogram do
